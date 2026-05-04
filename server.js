fe9da30cff76853ee331894156eff338b0d0c64e
assadiharis-cmd <assadiharis@gmail.com>
2026-05-04 14:58:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -42,11 +42,15 @@ function required(value) {
 function createTransporter() {
   return nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT || 465),
-    secure: String(process.env.SMTP_SECURE) === "true",
+    port: Number(process.env.SMTP_PORT || 587),
+    secure: false,
+    requireTLS: true,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
     },
     connectionTimeout: 10000,
     greetingTimeout: 10000,
